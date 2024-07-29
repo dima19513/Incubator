6af43e5c07f79f790f15aa2ef0f887959e5913ae
DimaBogdanovich <dimabogdanovich95@mail.ru>
2024-07-29 18:08:49 +0300

    del router

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import {SETTINGS} from "./settings";
 import {getVideosController} from './videos/getVideosController'
-import {videosRouter} from './index'
+import {createVideoController} from './videos/createVideoController'
+import {findVideoController} from './videos/findVideoController'
+import {deleteVideoController} from './videos/deleteVideoController'
+//import {videosRouter} from './index'
 
 export const app = express() // создать приложение
 app.use(express.json()) // создание свойств-объектов body и query во всех реквестах
@@ -12,5 +15,8 @@ app.get('/', (req, res) => {
     // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
     res.status(200).json({version: '1.0'})
 })
-//app.get(SETTINGS.PATH.VIDEOS, getVideosController)
-app.use(SETTINGS.PATH.VIDEOS, videosRouter)
+app.get(SETTINGS.PATH.VIDEOS, getVideosController)
+app.get(SETTINGS.PATH.VIDEOS, createVideoController)
+app.get(SETTINGS.PATH.VIDEOS, findVideoController)
+app.get(SETTINGS.PATH.VIDEOS, deleteVideoController)
+//app.use(SETTINGS.PATH.VIDEOS, videosRouter)
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import {app} from './app'
 import {SETTINGS} from './settings'
-
+/*
 import {Router} from 'express'
 import {getVideosController} from './videos/getVideosController'
 import {createVideoController} from './videos/createVideoController'
 import {findVideoController} from './videos/findVideoController'
 import {deleteVideoController} from './videos/deleteVideoController'
+
 
 export const videosRouter = Router()
 
@@ -13,7 +14,7 @@ videosRouter.get('/', getVideosController)
 videosRouter.post('/', createVideoController)
 videosRouter.get('/:id', findVideoController)
 videosRouter.delete('/:id', deleteVideoController)
-
+*/
 app.listen(SETTINGS.PORT, () => {
     console.log('...server started in port ' + SETTINGS.PORT)
 })
